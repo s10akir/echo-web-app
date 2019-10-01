8723051d135cfaf39acaa36b3a2727c20cda6749
Akira Shinohara <k017c1067@it-neec.jp>
2019-10-01 22:32:16 +0900

    [new] routing for task

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+
+	"github.com/s10akir/echo-web-app/src/controllers"
 )
 
 const PORT = ":8080"
@@ -14,6 +16,16 @@ func main() {
 	app.GET("/", func(context echo.Context) error {
 		return context.String(http.StatusOK, "Hello World!")
 	})
+
+	task := app.Group("/task")
+	{
+		taskController := controller.TaskController{}
+		task.GET("", taskController.Index)
+		task.POST("", taskController.New)
+		task.GET("/:id", taskController.Show)
+		task.POST("/:id", taskController.Update)
+		task.DELETE("/:id", taskController.Delete)
+	}
 
 	app.Start(PORT)
 }
