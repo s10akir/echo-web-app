cd5cbdc3295dbcf6ac469c8d2af3a583702144a4
Akira Shinohara <k017c1067@it-neec.jp>
2019-10-01 19:04:26 +0900

    [new] web server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/s10akir/echo-web-app
 
 go 1.13
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190927123631-a832865fa7ad // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,6 +1,19 @@
 package main
 
-import ()
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+const PORT = ":8080"
 
 func main() {
+	app := echo.New()
+
+	app.GET("/", func(context echo.Context) error {
+		return context.String(http.StatusOK, "Hello World!")
+	})
+
+	app.Start(PORT)
 }
